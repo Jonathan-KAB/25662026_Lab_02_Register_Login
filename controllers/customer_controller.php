5ca7347428760af013aa2189709eb8745d3f282e
Jonathan-KAB <jonathan.boateng@ashesi.edu.gh>
2025-09-19 19:23:26 +0000

    login functionality working as intended, testing to see if it's correctly parsing password and comparing to database string

--- controllers/customer_controller.php
+++ controllers/customer_controller.php
@@ -2,6 +2,7 @@
 
 require_once '../classes/customer_class.php';
 
+// added country and city parameters
 function register_user_ctr($name, $email, $password, $phone_number, $country, $city, $role)
 {
     $user = new User();
@@ -18,11 +19,19 @@ function get_user_by_email_ctr($email)
     return $user->getUserByEmail($email);
 }
 
+// ADD THIS FUNCTION - it's missing but your action file needs it
 function email_exists_ctr($email)
 {
     $user = new User();
     $existing_user = $user->getUserByEmail($email);
     return $existing_user !== false;
+}
+
+// ADD THIS FUNCTION - for login authentication
+function authenticate_user_ctr($email, $password)
+{
+    $user = new User();
+    return $user->authenticateUser($email, $password);
 }
 
 ?>
